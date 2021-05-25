639c6bc365101e6661f87d85129e716bf89de8cd
eslazyk10 <eslazy32@gmail.com>
2021-05-25 14:42:51 -0500

    added css and styling to page, used js battery function to get computer battery level and update battery level image and inner html

---
+++ index.js
@@ -0,0 +1,6 @@
+let percentage = document.querySelector('.percentage');
+let percent = document.querySelector('.percent');
+navigator.getBattery().then(function(battery) {
+    percentage.style.width = battery.level * 100 + '%';
+    percent.innerHTML = battery.level * 100 + '%';
+})
